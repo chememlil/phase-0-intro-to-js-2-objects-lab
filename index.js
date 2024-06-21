03e610c2290b0117862b2d30398cd810ea650e44
Levy Chemelil <levy.chelal@student.moringaschool.com>
2024-06-21 17:29:17 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+let employee = {
+    name: 'John Doe',
+    streetAddress: '123 Main St'
+  };
+  // Update employee object with a new key-value pair (non-destructive)
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value
+    };
+  }
+  
+  // Update employee object with a new key-value pair (destructive)
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  // Delete key-value pair from a clone of employee object (non-destructive)
+  function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  // Delete key-value pair from the employee object (destructive)
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
